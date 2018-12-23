dd59020b3a8dd1d16c236f5492fc2bfe21b069d9
h-inouchi <hibird2720804@gmail.com>
2018-12-24 04:12:58 +0900

    修正
    修正

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class ProfileController extends Controller
+    
     public function edit()　{
         return view('admin.profile.edit');
     }
