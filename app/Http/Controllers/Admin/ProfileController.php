e695fb7f39de66a3951a58f3809206b15a08066d
h-inouchi <hibird2720804@gmail.com>
2018-12-24 04:08:43 +0900

    インデント修正
    インデント修正

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -6,15 +6,11 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class ProfileController extends Controller
-{
-    //
-  public function edit()
-  {
-  return view('admin.profile.edit');
-  }
+    public function edit()　{
+        return view('admin.profile.edit');
+    }
 
-  public function update()
-  {
-  return redirect('admin/profile/edit');
-  }
+    public function update()　{
+        return redirect('admin/profile/edit');
+    }
 }
