ccc8cd1f675f6bb129c695f8bda0fbf2a5805cfa
N-Script <dna4570@gmail.com>
2018-12-24 13:05:24 +0900

    commit

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
-    Route::get('profile/edit' 'Admin\ProfileController@edit');
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
+    Route::get('news', 'Admin\NewsController@index')->middleware('auth');
+
+    // Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
